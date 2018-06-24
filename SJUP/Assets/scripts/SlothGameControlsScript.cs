0a270965fc7b9a80c39f64b2fb05133f17d591b6
brycefowler <40506964+brycefowler@users.noreply.github.com>
2018-06-23 21:39:56 -0700

    sloth Build Up Speed
    Rename "BryceTest" scene to "Sloth"
    Build up speed implemented, tweeking needed.
    Based on position milestones that scale with speed
    Small level  design work

--- SJUP/Assets/scripts/SlothGameControlsScript.cs
+++ SJUP/Assets/scripts/SlothGameControlsScript.cs
@@ -14,6 +14,8 @@ public class SlothGameControlsScript : MonoBehaviour
     public float moveForce { get; set; }
     public float speed = 10f;
     public float jumpForce = 100f;
+    public float speedUpCheckpoint = 1000f;
+    public float speedMultiplier = 1.5f;
     public Transform groundCheck;
 
     private Collider2D myCollider;
@@ -22,6 +24,7 @@ public class SlothGameControlsScript : MonoBehaviour
     //private bool grounded = false;
     private Animator anim;
     private Rigidbody2D rb2d;
+    private float speedUpCount = 0;
 
     // Use this for initialization
     void Start()
@@ -35,10 +38,17 @@ public class SlothGameControlsScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x > speedUpCount)
+        {
+            speed *= speedMultiplier;
+            speedUpCheckpoint *= speedMultiplier;
+            speedUpCount += speedUpCheckpoint;
+            
+        }
         grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
 
-
         rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
+
         if (Input.GetKeyDown(KeyCode.Space) /*&& grounded*/)
         {
             jump = true;
@@ -62,11 +72,6 @@ public class SlothGameControlsScript : MonoBehaviour
                 rb2d.velocity = new Vector2(rb2d.velocity.x, Mathf.Sign(rb2d.velocity.y) * 0.2f);
             jump = false;
             
-        }
-
-        if (Mathf.Sign(rb2d.velocity.x) > 15f)
-        {
-            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * 0.995f, rb2d.velocity.y);
         }
     }
 }
