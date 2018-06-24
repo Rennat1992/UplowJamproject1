b0880c0032244bf3b8e2e14f627c1c2c2dccdc38
iwormbot <40480105+iwormbot@users.noreply.github.com>
2018-06-24 14:17:38 -0700

    Some incomplete coding on Enemy NPC and Character (can be removed if need be)

---
+++ SJUP/Assets/scripts/GluttonyBodyBitsPlugins.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GluttonyBodyBitsPlugins : MonoBehaviour
+{
+    public float Damage = 10;
+    public float fireRate = 0;
+    public LayerMask notToHit;
+
+    float timeToFire = 0;
+    Transform firePoint;
+
+    //Use this for initialization
+    void Awake ()
+    {
+        firePoint = transform.Find ("FirePoint");
+        if (firePoint == null)
+        {
+            Debug.LogError("No firePoint? WHAT!?");
+        }
+    }
+    //Update is caleld once per frame
+    void Update ()
+    {
+        if (fireRate == 0)
+        {
+            if (Input.GetKeyDown (KeyCode.Alpha1))
+            {
+                Shoot();
+            }
+        }
+        else
+        {
+            if (Input.GetButton ("Fire1") && timeToFire.time > timeToFire)
+            {
+                timeToFire = timeToFire.time + 1 / fireRate;
+                shoot();
+            }
+        }
+    }
+    void Shoot () {
+      }
+}
--- SJUP/Assets/scripts/GluttonyPatrol.cs
+++ SJUP/Assets/scripts/GluttonyPatrol.cs
@@ -4,36 +4,28 @@ using UnityEngine;
 
 public class GluttonyPatrol : MonoBehaviour
 {
-    public float speed = 5;
+    public float speed = 1;
     public float time = 1;
     public float distance = 1;
-
+    public Transform transform;
+              
     private bool movingRight = true;
     private bool movingLeft = true;
 
-    public Transform groundDetection;
+    // This is our direction we're travelling in.
+    Vector3 direction = new Vector3(1, 0, 0);
 
+        // Move it along its direction.
     void Update()
     {
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
-
-        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
-        if (groundInfo.collider == false)
-        {
-            transform.eulerAngles = new Vector3(0, 180, 0);
-            movingRight = true;
-            movingLeft = false;
-        }
-        else if (groundInfo.collider == true)
-        {
-            transform.eulerAngles = new Vector3(0, -180, 0);
-            movingRight = false;
-        }
-
- 
-            
-
-        
-
-        }
+        transform.Translate(direction.x * Time.deltaTime, 0,0);
+    }
+    
+    // If we hit a left or right wall, invert x direction.
+    void OnCollisionEnter2D(Collision2D col)
+    {
+     // bump = collisionInfo.Gluttony.tag;
+        if (col.transform.name == "wallRight" || col.transform.name == "wallLeft")
+            direction.x *= -1;
+    }
 }
