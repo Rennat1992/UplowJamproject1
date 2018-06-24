6958ae4f32c052c1f04b840c664ed8b3cce8fb0b
brycefowler <40506964+brycefowler@users.noreply.github.com>
2018-06-24 16:52:49 -0700

    Working on death mechanics
    Still WIP
    Falling death works, walls don't
    Commented my trash code

---
+++ SJUP/Assets/scripts/SlothGameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlothGameManager : MonoBehaviour {
+
+    public SlothPlayerController player;
+
+    private Vector2 playerStartPoint;
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerStartPoint = player.transform.position;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //public void RestartGame ()
+    //{
+    //    StartCoroutine (RestartGameCo ());
+
+    //}
+
+    //public IEnumerator RestartGameCo ()
+    //{
+    //    player.GameObject.setActive(false);
+    //    yield return new WaitForSeconds(1f);
+    //    player.transform.position = playerStartPoint;
+    //    player.GameObject.setActive(true);
+    //}
+}
---
+++ SJUP/Assets/scripts/SlothPlayerController.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlothPlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpForce;
+    public float inAirForce;
+    public float speedUpCheckpoint;
+    public float speedMultiplier;
+    public float jumpTime;
+    public float deathHeight;
+
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+    public LayerMask whatIsDeath;
+    public Collider2D myCollider;
+    public Rigidbody2D rb2d;
+    public SlothGameManager gameManager;
+
+
+    public bool grounded = false;
+    public bool moving = false;
+
+    private Animator anim;
+    private float speedUpCount;
+    private float jumpTimeCount;
+    private Vector2 playerStartPoint;
+    public float oldXPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
+        myCollider = GetComponent<Collider2D>();
+        speedUpCount = speedUpCheckpoint;
+        jumpTimeCount = jumpTime;
+        playerStartPoint = rb2d.position;
+        oldXPosition = rb2d.position.x - 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Update velocity
+        rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
+
+        //Check if player is moving or stopped by wall - BROKEN
+        //moving = (rb2d.position.x > oldXPosition);
+
+        //checks if player died
+        if ((rb2d.position.y < deathHeight) || !moving)
+        {
+            StartCoroutine(RestartGameCo());
+        }
+
+        //Checks if player is on the ground
+        grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+        
+        //If player reaches checkpoint, scale the speed and distance to next checkpoint
+        if (rb2d.position.x > speedUpCount)
+        {
+            speed *= speedMultiplier;
+            speedUpCheckpoint *= speedMultiplier;
+            speedUpCount += speedUpCheckpoint;
+        }
+
+        //Checks if player presses jump and is able to jump
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            rb2d.AddForce(new Vector2(rb2d.velocity.x, jumpForce));
+        }
+
+        //Checks if player holds down space for long jump
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (jumpTimeCount > 0)
+            {
+                rb2d.AddForce(new Vector2(rb2d.velocity.x, inAirForce));
+                jumpTimeCount -= Time.deltaTime;
+            }
+        }
+
+        //Checks if player releases space bar to end long jump
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            jumpTimeCount = 0;
+        }
+
+        //Checks if player is grounded to reset long jump
+        if (grounded)
+        {
+            jumpTimeCount = jumpTime;
+        }
+
+        //Update old position to try to check for death by wall
+        oldXPosition = rb2d.position.x;
+    }
+
+
+    //Stuff for restarting the game post death
+    public IEnumerator RestartGameCo()
+    {
+        rb2d.velocity = new Vector2(0, 0);
+        yield return new WaitForSeconds(2f);
+        rb2d.position = playerStartPoint;
+        rb2d.velocity = new Vector2(speed, 0);
+        oldXPosition = rb2d.position.x - 1;
+    }
+}
