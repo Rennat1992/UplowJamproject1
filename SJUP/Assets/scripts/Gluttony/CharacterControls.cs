00c99de9e9e4db6b9b2a0be5dda109f7a59a4b1e
iwormbot <40480105+iwormbot@users.noreply.github.com>
2018-06-25 23:29:50 -0700

    Pushing broken bullet script

---
+++ SJUP/Assets/scripts/Gluttony/CharacterControls.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControls : MonoBehaviour
+{
+    [SerializeField]
+public class Boundary
+    {
+        public float xMin, xMax;
+    }
+    private float moveSpeed = 5f;
+    public Boundary boundary;
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private float nextFire;
+
+    //To allow Player to shoot
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+            nextFire = Time.time + fireRate;
+ //           GameObject clone = 
+                Instantiate(shot, shotSpawn.position, shotSpawn.rotation); // as GameObject
+    }
+
+   
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+        }
+    }
+}
