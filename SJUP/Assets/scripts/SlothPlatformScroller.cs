8b05d02386e3246f5d321a0ee256dbb8c436bba0
brycefowler <40506964+brycefowler@users.noreply.github.com>
2018-06-25 21:47:32 -0700

    Deep in the shit
    Adding scripts for platform generator

---
+++ SJUP/Assets/scripts/SlothPlatformScroller.cs
@@ -0,0 +1,32 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class SlothPlatformScroller : MonoBehaviour {
+
+//    public float scrollSpeed = 5.0f;
+
+//	// Use this for initialization
+//	void Start () {
+		
+//	}
+	
+//	// Update is called once per frame
+//	void Update ()
+//    {
+//        //Scrolling
+//        GameObject currentChild;
+//        for (int i = 0; i < tansform.childCount; i++)
+//        {
+//            currentChild = tansform.GetChild(i).gameObject;
+//            ScrollPlatform (currentChild);
+//        }
+//	}
+
+
+//    void ScrollPlatform (GameObject currentPlatform)
+//    {
+//        currentPlatform.tansform.position -= Vector3.right * (scrollSpeed * Time.deltaTime);
+
+//    }
+//}
