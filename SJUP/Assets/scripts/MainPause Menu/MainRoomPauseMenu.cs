f60469dff8e8f3aeb5da08e93798e79010dd2d28
brobear988 <ultima988@gmail.com>
2018-06-26 20:07:01 -0700

    Added basic pause menu to Main Room

---
+++ SJUP/Assets/scripts/MainPause Menu/MainRoomPauseMenu.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainRoomPauseMenu : MonoBehaviour
+{
+
+
+    //Canvas Element
+    public Canvas Pause;
+
+    //puase player control
+    public MainRoomControls playP;
+
+    bool paused = false;
+
+
+    void Start()
+    {
+        playP = GameObject.Find("Player").GetComponent<MainRoomControls>();
+        Pause = GameObject.Find("PauseMenu").GetComponent<Canvas>();
+        Pause.enabled = false;
+        playP.enabled = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            playP.enabled = !playP.enabled;
+            Pause.enabled = !Pause.enabled;
+            paused = togglePause();
+        }
+    }
+
+    bool togglePause()
+    {
+        if (Time.timeScale == 0f)
+        {
+            Time.timeScale = 1f;
+            return (false);
+        }
+        else
+        {
+            Time.timeScale = 0f;
+            return (true);
+        }
+    }
+}
---
+++ SJUP/Assets/scripts/MainPause Menu/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+
+
+    //Canvas Element
+    public Canvas Pause;
+
+    //puase player control
+    public GameObject playP;
+
+    bool paused = false;
+
+
+    void Start()
+    {
+        Pause  = GameObject.Find("PauseMenu").GetComponent<Canvas>();
+        Pause.enabled = false;
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause.enabled = !Pause.enabled;
+            paused = togglePause();
+        }
+    }
+
+    bool togglePause()
+    {
+        if (Time.timeScale == 0f)
+        {
+            Time.timeScale = 1f;
+            return (false);
+        }
+        else
+        {
+            Time.timeScale = 0f;
+            return (true);
+        }
+    }
+}
