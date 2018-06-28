4b6589356a6bca2ddb065ac6edc48da323dce984
brycefowler <40506964+brycefowler@users.noreply.github.com>
2018-06-28 01:46:02 -0700

    Sloth Random Height
    Added heights to platforms
    TO DO: Fix dumb dumb stupid issue with not destroying all the platforms

--- SJUP/Assets/scripts/SlothPlatformGenerator.cs
+++ SJUP/Assets/scripts/SlothPlatformGenerator.cs
@@ -41,17 +41,28 @@ public class SlothPlatformGenerator : MonoBehaviour
 
     public SlothObjectPooler[] theObjectPools;
 
+
+    private float minHeight;
+    public Transform maxHeightPoint;
+    private float maxHeight;
+    public float maxHeightChange;
+    private float heightChange;
+
     //Use this for initialization
 
     void Start()
     {
         //platformWidths[i] = theObjectPools[i].pooledObject.GetComponent<BoxCollider2D>().size.x;
+        platformWidths = new float[theObjectPools.Length];
 
         for (int i = 0; i < theObjectPools.Length; i++)
         {
-            platformWidths[i] = theObjectPools[i].GetComponent<BoxCollider2D> ().size.x;
+            platformWidths[i] = theObjectPools[i].pooledObject.GetComponent<BoxCollider2D> ().size.x;
 
         }
+
+        minHeight = transform.position.y;
+        maxHeight = maxHeightPoint.position.y;
     }
 
     //Update is called once per frame
@@ -62,9 +73,22 @@ public class SlothPlatformGenerator : MonoBehaviour
         {
             distanceBetween = Random.Range (distanceBetweenMin, distanceBetweenMax);
 
-            transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
-
             platformSelector = Random.Range(0, theObjectPools.Length);
+
+            heightChange = transform.position.y + Random.Range (-maxHeightChange, maxHeightChange);
+
+            if (heightChange > maxHeight)
+            {
+                heightChange = maxHeight;
+            }
+            if (heightChange < minHeight)
+            {
+                heightChange = minHeight;
+            }
+
+            transform.position = new Vector3(transform.position.x + (platformWidths[platformSelector] / 2) + distanceBetween, heightChange, transform.position.z);
+
+            //platformSelector = Random.Range(0, theObjectPools.Length);
 
             //Instantiate (platform, transform.position, transform.rotation);
             GameObject newPlatform = theObjectPools[platformSelector].GetPooledObject ();
@@ -72,6 +96,8 @@ public class SlothPlatformGenerator : MonoBehaviour
             newPlatform.transform.position = transform.position;
             newPlatform.transform.rotation = transform.rotation;
             newPlatform.SetActive (true);
+
+            transform.position = new Vector3(transform.position.x + (platformWidths[platformSelector] / 2), transform.position.y, transform.position.z);
         }
     }
 }
