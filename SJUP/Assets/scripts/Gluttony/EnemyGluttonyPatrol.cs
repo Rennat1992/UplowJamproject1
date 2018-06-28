3ec6f746ab6d07307f2572abbb109ca9242f5f53
iwormbot <40480105+iwormbot@users.noreply.github.com>
2018-06-27 20:04:28 -0700

    Incomplete Enemy AI script...again

--- SJUP/Assets/scripts/Gluttony/EnemyGluttonyPatrol.cs
+++ SJUP/Assets/scripts/Gluttony/EnemyGluttonyPatrol.cs
@@ -4,42 +4,27 @@ using UnityEngine;
 
 public class EnemyGluttonyPatrol : MonoBehaviour
 {
-    public Transform[] patrolPoints;
     public float speed;
-    public float distance;
 
     private bool movingRight = true;
 
     public Transform groundDetection;
-    public Vector2 direction;
 
-    public float yRotation = 5.0F;
-    //Use this for initialization
-
-    void Start() {
-
-    }
-
-    void Update()
-    {
-        transform.Translate(Vector2.right * Time.deltaTime * speed);
-        //Cast a ray in the direction specified in the inspector.
-                RaycastHit2D hit = Physics2D.Raycast(groundDetection.gameObject.transform.position, Vector2.down, distance);
-        if(groundInfo.collider == false)
-        {
-            if (movingRight == true)
-            {transform.eulerAngles = new Vector3(0, -180, 0);
-                movingRight = false;
+    void Update() {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        int layer_mask = LayerMask.GetMask("GroundInfo");
+        RayCastHit2D groundInfo = Physics2D.RayCast(groundDetection, Vector2.down, 10f);
+        if (groundInfo.collider.gameObject.tag == "ground") == false){
+                if (movingRight == true)
+                {
+                    transform.eulerAngles = new Vector3(0, -180, 0);
+                    movingRight = false;
+                } else
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                    movingRight = true;
+                }
             }
-            else
-            {
-             transform.eulerAngles = new Vector3(0, 0, 0);
-                movingRight = false;
-
-            }
-
         }
-    }
-
-
-}
+ }
+  
