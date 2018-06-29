ee3ac67babcf172558bc9663c0c3057bf2b706b7
iwormbot <40480105+iwormbot@users.noreply.github.com>
2018-06-29 06:17:34 -0700

    Incomplete Health stats for Gluttony

---
+++ SJUP/Assets/scripts/Gluttony/DestroyByContactG.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContactG : MonoBehaviour {
+    void OnTriggerEnter(Collider other)
+    {   Debug.Log (other.name);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
---
+++ SJUP/Assets/scripts/Gluttony/HealthScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class EnemyStats
+    {
+        public int Health = 13;
+        public float fireRate = 1.5f;
+    }
+
+    public EnemyStats stats = new EnemyStats();
+
+    public void DamageEnemy(int damage)
+    {
+        stats.Health -= damage;
+        if (stats.Health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
