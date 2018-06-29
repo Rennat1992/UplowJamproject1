e14c6dc59c526ed19a4a14dd32728170c51a3474
brobear988 <ultima988@gmail.com>
2018-06-28 23:25:28 -0700

    Added Slots to Main World and changed movement
    Character is frozen when doing slots

---
+++ SJUP/Assets/scripts/MainRoom/SlotScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotScript : MonoBehaviour {
+
+    private CanvasGroup SlotCanvas;
+
+
+    // Use this for initialization
+    void Start () {
+        SlotCanvas = GameObject.Find("SlotCanvas").GetComponent<CanvasGroup>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            SlotCanvas.alpha = 1;
+            SlotCanvas.blocksRaycasts = true;
+            SlotCanvas.interactable = true;
+            GreedSpinToWin.GreedSpinToWinSingle.slotMachine = true;
+
+            GameObject.Find("Player").GetComponent<MainRoomControls>().enabled = false;
+        }
+    }
+}
