ffb818308599a91f5b46666603ceddd84084fc73
brobear988 <ultima988@gmail.com>
2018-06-28 23:03:36 -0700

    MainHub Collision Completed
    All Walls and floors have collision yay!

---
+++ SJUP/Assets/scripts/MainRoom/WallHelper4.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallHelper4 : MonoBehaviour
+{
+
+    //change player position
+    public Transform play;
+
+    // Use this for initialization
+    void Start()
+    {
+        play = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            WallAlpha4.WallAlpha4Single.enter = false;
+
+            //behind walls
+            play.position = new Vector3(GameObject.Find("Player").GetComponent<Transform>().position.x, GameObject.Find("Player").GetComponent<Transform>().position.y, -4);
+            GameObject.Find("Player").GetComponent<Transform>().position = play.position;
+        }
+    }
+}
