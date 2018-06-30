1edd583f5b3dd4f1e98d37fb0b66dc7b640304f9
brobear988 <ultima988@gmail.com>
2018-06-30 10:10:34 -0700

    Working on Main Room Animations
    struggling =/

---
+++ SJUP/Assets/scripts/MainRoom/MainRoomAnimator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainRoomAnimator : MonoBehaviour
+{
+    public Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+    }
+}
